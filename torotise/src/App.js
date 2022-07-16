ab6034e512c2df927337c97355e7f5d3d717846c
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-07-16 17:16:00 +0530

    ready

--- torotise/src/App.js
+++ torotise/src/App.js
@@ -1,14 +1,84 @@
-// import logo from './logo.svg';
-import './App.css';
-import {Layout} from "./Components/layout.jsx"
+import { useEffect, useRef, useState } from "react";
+import { Layout } from "./Components/layout";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState("");
+  const [timer, setTimer] = useState(0);
+  const [userInput, setUserInput] = useState("");
+  const timerRef = useRef(null);
+  const [hasGameEnded, setHasGameEnded] = useState(false);
+  const [record, setRecord] = useState(0);
+
+  let prev_record = localStorage.getItem("score_record");
+  // setRecord(prev_record);
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const handleReset = () => {
+    setTimer(0);
+    setUserInput("");
+    setHasGameEnded(false)
+    timerRef.current = clearInterval(timerRef.current);
+  };
+
+  const startTimer = () => {
+    if (!timerRef.current) {
+      timerRef.current = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    const randomInt = randomIntFromInterval(65, 90);
+    const newVal = String.fromCharCode(randomInt);
+    setValue(newVal);
+  }, [userInput]);
+
+
+  useEffect(() => {
+    if (userInput.trim()) {
+      setRecord(prev_record);
+      if (userInput.length !== 20 && !hasGameEnded) {
+        startTimer();
+        const latestInput = userInput[userInput.length - 1]?.toUpperCase();
+        if (latestInput !== value) {
+          setTimer((prev) => prev + 0.5);
+        }
+      } else {
+        // console.log(prev_record);
+        
+        if(timer < prev_record){
+          localStorage.removeItem("score_record");
+          localStorage.setItem("score_record", timer);
+          setRecord(timer);
+        }
+        timerRef.current = clearInterval(timerRef.current);
+        if(timer > prev_record){
+          setValue("GAME OVER");
+        }else{
+          setValue("Success");
+        }
+        
+        setHasGameEnded(true);
+      }
+    }
+  }, [userInput]);
+
   return (
     <div className="App">
-      <Layout></Layout>
-      
+      <Layout
+        userInput={userInput}
+        setUserInput={setUserInput}
+        value={value}
+        timer={timer}
+        handleReset={handleReset}
+        recorddata = {prev_record>0 ? prev_record : 0}
+      />
     </div>
   );
 }
 
-export default App;
--- torotise/src/Components/layout.jsx
+++ torotise/src/Components/layout.jsx
@@ -1,49 +1,60 @@
-import "./layout.css"
-import {GiNetworkBars} from "react-icons/gi";
-import {FaWifi, FaRegCircle} from "react-icons/fa";
-import {BsBatteryFull} from "react-icons/bs";
-import {MdArrowBackIos} from "react-icons/md";
-import {BiSquareRounded} from "react-icons/bi"
+import "./layout.css";
+import { GiNetworkBars } from "react-icons/gi";
+import { FaWifi, FaRegCircle } from "react-icons/fa";
+import { BsBatteryFull } from "react-icons/bs";
+import { MdArrowBackIos } from "react-icons/md";
+import { BiSquareRounded } from "react-icons/bi";
 
-
-
-
-export const Layout = ( ) =>{
-    return (
-        <div className="container">
-            <div className="navbar">
-                <div className="time">
-                    <p className="clock">9:41</p>
-                </div>
-                <div className="battery">
-                    <GiNetworkBars className="network"></GiNetworkBars>
-                    <FaWifi className="wifi"></FaWifi>
-                    <BsBatteryFull className="cell"></BsBatteryFull>
-                
-                </div>
-            </div>
-            <div className="info">
-                <h3 className="bigtext">Type The Alphabet</h3>
-                <p className="smalltext">Typing game to see how fast you type. Timer starts when you do :)</p>
-            </div>
-            <div className="display">
-                <p className="typed">Success!</p>
-            </div>
-            <div className="timer_record">
-                <p className="current_timer">Time: 10</p>
-                <p className="past_record">my best time: 1.39s!</p>
-            </div>
-            <div className="input_div">
-                <input className="inputbar" placeholder="Type Here"></input>
-                <button className="reset">Reset</button>
-            </div>
-            <div className="footer">
-                <MdArrowBackIos className="buttons"></MdArrowBackIos>
-                <FaRegCircle className="buttons"></FaRegCircle>
-                <BiSquareRounded className="buttons"></BiSquareRounded>
-
-            </div>
+export const Layout = ({
+  value,
+  timer,
+  userInput,
+  setUserInput,
+  handleReset,
+  recorddata
+}) => {
+  return (
+    <div className="container">
+      <div className="navbar">
+        <div className="time">
+          <p className="clock">9:41</p>
         </div>
-    )
-
-}
+        <div className="battery">
+          <GiNetworkBars className="network"></GiNetworkBars>
+          <FaWifi className="wifi"></FaWifi>
+          <BsBatteryFull className="cell"></BsBatteryFull>
+        </div>
+      </div>
+      <div className="info">
+        <h3 className="bigtext">Type The Alphabet</h3>
+        <p className="smalltext">
+          Typing game to see how fast you type. Timer starts when you do :)
+        </p>
+      </div>
+      <div className="display">
+        <p className="typed">{value}</p>
+      </div>
+      <div className="timer_record">
+        <p className="current_timer">Time: {timer}</p>
+        <p className="past_record">my best time: {recorddata}!</p>
+      </div>
+      <div className="input_div">
+        <input
+          className="inputbar"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          placeholder="Type Here"
+          autoFocus
+        ></input>
+        <button onClick={handleReset} className="reset">
+          Reset
+        </button>
+      </div>
+      <div className="footer">
+        <MdArrowBackIos className="buttons"></MdArrowBackIos>
+        <FaRegCircle className="buttons"></FaRegCircle>
+        <BiSquareRounded className="buttons"></BiSquareRounded>
+      </div>
+    </div>
+  );
+};
